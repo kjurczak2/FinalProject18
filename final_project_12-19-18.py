85ca2c336d432e9f0b5385f054f8b38aa2784c01
kjurczak2 <kjurczak@ucvts.org>
2018-12-19 18:05:21 +0000

    conway's game of life: rules

--- final_project_12-19-18.py
+++ final_project_12-19-18.py
@@ -1,1 +1,6 @@
-print("hello")
+#Conway's Game of Life rules (found on Wikipedia):
+    #1: Live cell with fewer than two neighbors dies (underpopulation)
+    #2: Live cell with two or three neighbors lives
+    #3: Live cell with more than three neighbors dies (overpopulation)
+    #4: Dead cell with exactly three neighbors becomes a live cell (reproduction)
+
