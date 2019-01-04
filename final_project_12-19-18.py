5c94e4c61a6b2d47c76a22b96a0aafc3302a4b01
kjurczak2 <kjurczak@ucvts.org>
2019-01-04 17:19:39 +0000

    conway's game of life - basic grid setup

--- final_project_12-19-18.py
+++ final_project_12-19-18.py
@@ -4,3 +4,32 @@
     #3: Live cell with more than three neighbors dies (overpopulation)
     #4: Dead cell with exactly three neighbors becomes a live cell (reproduction)
 
+import random
+
+rows = 10
+cols = 10
+
+class Cell:
+    def __init__(self, x, y, alive):
+        self.x = x
+        self.y = y
+        self.alive = alive
+    def __str__(self):
+        return f"{self.x}, {self.y}: {self.alive}"
+    __repr__ = __str__
+
+cells = []
+
+for y in range(rows):
+    row = []
+    for x in range(cols):
+        row.append(Cell(x, y, random.choice([True, False])))
+    cells.append(row)
+
+for row in cells:
+    for cell in row:
+        if cell.alive:
+            print("X", end = " | ")
+        else:
+            print(" ", end = " | ")
+    print(" ")
