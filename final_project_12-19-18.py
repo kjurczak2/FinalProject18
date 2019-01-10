0705e4e33e24e71c923d20635f092fda662ef285
kjurczak2 <kjurczak@ucvts.org>
2019-01-10 18:24:15 +0000

    check_neighbors and neighbors_status working

--- final_project_12-19-18.py
+++ final_project_12-19-18.py
@@ -14,6 +14,51 @@ class Cell:
         self.x = x
         self.y = y
         self.alive = alive
+        self.north = self.y - 1
+        if self.y == 0:
+            self.north = cols - 1
+        self.east = self.x + 1
+        if self.x == rows - 1:
+            self.east = 0
+        self.south = self.y + 1
+        if self.y == cols - 1:
+            self.south = 0
+        self.west = self.x - 1
+        if self.x == 0:
+            self.west = rows - 1
+
+
+    def check_neighbors(self):
+        """Check neighboring cells' status when given the center cell"""
+        #cell coordinates: (y,x)
+        #rows: (0,0) (0,1) (0,2) . . .
+        #cols: (0,0) (1,0) (2,0) . . .
+        n = ((self.north), (self.x))
+        ne = ((self.north), (self.east))
+        e = ((self.y), (self.east))
+        se = ((self.south), (self.east))
+        s = ((self.south), (self.x))
+        sw = ((self.south), (self.west))
+        w = ((self.y), (self.west))
+        nw = ((self.north), (self.west))
+        locations = [n, ne, e, se, s, sw, w, nw]
+        neighbors = []
+        print(f"getting neighbors of {self.y} {self.x} now")
+        for loc in locations:
+            print(loc)
+            neighbors.append(cells[loc[0]][loc[1]])
+        return neighbors
+
+    def neighbors_status(self, neighbor_cells):
+        """Check status of neighboring cells (either alive or dead)"""
+        live_neighbors = 0
+        for n in neighbor_cells:
+            if n.alive == True:
+                live_neighbors += 1
+        print(live_neighbors)
+        return live_neighbors
+
+
     def __str__(self):
         return f"{self.x}, {self.y}: {self.alive}"
     __repr__ = __str__
@@ -21,6 +66,7 @@ class Cell:
 cells = []
 
 for y in range(rows):
+    print("Making a row")
     row = []
     for x in range(cols):
         row.append(Cell(x, y, random.choice([True, False])))
@@ -34,18 +80,7 @@ for row in cells:
             print(" ", end = " | ")
     print(" ")
 
-def check_neighbors(self):
-    """Check neighboring cells' status when given the center cell"""
-    #cell coordinates: (y,x)
-    #rows: (0,0) (0,1) (0,2) . . .
-    #cols: (0,0) (1,0) (2,0) . . .
-    n = ((self.y - 1), (self.x))
-    ne = ((self.y - 1), (self.x + 1))
-    e = ((self.y), (self.x + 1))
-    se = ((self.y + 1), (self.x + 1))
-    s = ((self.y + 1), (self.x))
-    sw = ((self.y + 1), (self.x - 1))
-    w = ((self.y), (self.x - 1))
-    nw = ((self.y - 1), (self.x - 1))
-    neighbors = [n, ne, e, se, s, sw, w, nw]
-    return neighbors
+for row in cells:
+    for cell in row:
+        neighbors = cell.check_neighbors()
+        cell.neighbors_status(neighbors)
